6bafcd4788bcb16c572d509fd5a11d3c49f3d9c8
Sampurno Sarkar <sampurnosarkar.ind@gmail.com>
2025-06-21 03:24:51 +0530

    working state initial

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+    return (
+        <View className="flex-1 items-center justify-center bg-zinc-900">
+            <Text className="text-white text-2xl font-bold">🎬 Tailwind is Working!</Text>
+        </View>
+    );
+}
